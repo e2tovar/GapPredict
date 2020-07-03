b797833a6cd6e6e2f1f879434f1e297f369cfbc7
e2tovar <you@example.come2varadero@gmail.com>
2020-07-03 10:55:40 +0200

    Added 'Main.R'

---
+++ miscellaneous/Main.R
@@ -0,0 +1,22 @@
+#Loading require libraries
+lapply(c('XML', 'logging'), require, character.only = TRUE)
+
+directorio <- "D:/Estudio/BootCamp/R_Course/GapPredict/"
+
+#set directory
+setwd(directorio)
+
+#getting scripts from R file
+archivos = list.files(path = 'R/', recursive = TRUE)
+
+#Applying all functions 
+lapply(paste0("R/", archivos), source)
+
+
+
+#debug(clasificarContactosApp)
+#clasificarContactosApp(directorio)
+#undebug(clasificarContactosApp)
+
+
+  
